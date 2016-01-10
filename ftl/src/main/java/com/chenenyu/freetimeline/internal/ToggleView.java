0a5af321baddf7dc7ec3e30f7da077a85d644b7a
chenenyu <echocey@gmail.com>
2016-01-10 11:41:41 +0800

    recycle bitmap, and optimize memory

--- ftl/src/main/java/com/chenenyu/freetimeline/internal/ToggleView.java
+++ ftl/src/main/java/com/chenenyu/freetimeline/internal/ToggleView.java
@@ -1,6 +1,7 @@
 package com.chenenyu.freetimeline.internal;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.util.AttributeSet;
@@ -12,8 +13,9 @@ import android.view.View;
  */
 public class ToggleView extends View {
     private static final Paint togglePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    private boolean opened;
     private int toggleColor = FreeTimeLineUI.DEFAULT_TOGGLE_COLOR;
+    private boolean opened;
+    private Bitmap cache;
 
     public ToggleView(Context context) {
         this(context, null);
@@ -37,19 +39,38 @@ public class ToggleView extends View {
     protected void onDraw(Canvas canvas) {
         int width = this.getWidth();
         int height = this.getHeight();
-        int halfHeight = height / 2;
-        int halfWidth = width / 2;
-        if (this.opened) {
-            canvas.drawLine(0.0F, (float) halfHeight, (float) width, (float) halfHeight, togglePaint);
-        } else {
-            canvas.drawLine(0.0F, (float) halfHeight, (float) width, (float) halfHeight, togglePaint);
-            canvas.drawLine((float) halfWidth, 0.0F, (float) halfWidth, (float) height, togglePaint);
+
+        if (this.cache != null && (this.cache.getWidth() != width || this.cache.getHeight() != height)) {
+            this.cache.recycle();
+            this.cache = null;
+        }
+        if (cache == null) {
+            cache = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            Canvas cacheCanvas = new Canvas(cache);
+            int halfHeight = height / 2;
+            int halfWidth = width / 2;
+            if (this.opened) {
+                cacheCanvas.drawLine(0.0F, (float) halfHeight, (float) width, (float) halfHeight, togglePaint);
+            } else {
+                cacheCanvas.drawLine(0.0F, (float) halfHeight, (float) width, (float) halfHeight, togglePaint);
+                cacheCanvas.drawLine((float) halfWidth, 0.0F, (float) halfWidth, (float) height, togglePaint);
+            }
+        }
+
+        canvas.drawBitmap(this.cache, 0.0F, 0.0F, null);
+    }
+
+    private void recycleCache() {
+        if (this.cache != null) {
+            this.cache.recycle();
+            this.cache = null;
         }
     }
 
     public void setOpened(boolean opened) {
         if (opened != this.opened) {
             this.opened = opened;
+            recycleCache();
             this.invalidate();
         }
     }
@@ -58,6 +79,7 @@ public class ToggleView extends View {
         if (color != this.toggleColor) {
             toggleColor = color;
             togglePaint.setColor(toggleColor);
+            recycleCache();
             this.invalidate();
         }
     }
